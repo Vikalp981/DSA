3696cc2ec0515b5466189a240b8f05b58f36e672
Vikalp981 <tripathivikalp862@gmail.com>
2021-10-22 16:23:27 +0530

    first

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,26 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+//creating structure
 struct node{
     int data;
     struct node *next;
 
 }*head;
-void displayList();
-void insert_at_b(int data);
-void delete_at_m();
-void delete_at_e();
-void delete_at_b();
-void insert_at_m(int data, int position);
-void insert_at_e(int data);
-int search(int key);
-int count=0;
+//declaring all used functions as globally so that it can be accessed easily
+void displayList();//function to display linked list
+void insert_at_b(int data);//fuction to insert at beginning of linked list
+void delete_at_m();//function to delete the element in a specified position
+void delete_at_e();//function to delete last element
+void delete_at_b();//function to delete the beginning element
+void insert_at_m(int data, int position);//function to insert the element at a particular specified position
+void insert_at_e(int data);//function to insert the element at the end position
+int search(int key);//function to search the element in linked list
+int count=0;//used to find the length of the linked list
+//main function
 int main(){
-    struct node  *newnode , *temp;
+    struct node  *newnode , *temp;//declaring two pointers for accessing the data and address in the linked list
     head=0;
     int choice, opt_menu, key, data, position, index;
+    //entering elements in the linked list
     while(choice){
-        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode = (struct node *)malloc(sizeof(struct node));//using malloc to typecast it into pointer
         printf("Enter data \n");
         scanf("%d",&newnode->data);
         newnode->next=0;
@@ -31,14 +34,15 @@ int main(){
             temp->next=newnode;
             temp=newnode;
         }
+        //asking user to whether he wants to continue or not
         printf("Do you want to continue?\n");
         scanf("%d",&choice);
         count++;
 
     }
-    //displayList();
+    //display the menu for user();
     while(1)
-    {     
+    {    
         printf("Menu\n");
         printf("1. Insert at beginning\n");
         printf("2. Insert at middle\n");
@@ -48,19 +52,20 @@ int main(){
         printf("6. Delete at end\n");
         printf("7. Search an element\n");
         printf("8. exit\n");
+        //asking user what he wants to do
         printf("Enter your choice : ");
         scanf("%d", &opt_menu);
         printf("\n");
-        
+        //using switch to call functions according to user demands
         switch(opt_menu)
         {
-            case 1 :    
+            case 1 :    //calling function to insert at beginning
                         printf("\nEnter data to insert at beginning of the list: ");
                         scanf("%d", &data);
                         insert_at_b(data);
                         displayList();
                         break;
-            case 2 : 
+            case 2 :    //calling function to insert at middle
                         printf("Enter data to insert at middle of the list: ");
                         scanf("%d", &data);
                         printf("Enter the position to insert new node: " );
@@ -68,25 +73,25 @@ int main(){
                         insert_at_m(data, position);
                         displayList();
                         break;
-            case 3 : 
+            case 3 :    //calling function to insert element at end
                         printf("\nEnter data to insert at end of the list: ");
                         scanf("%d", &data);
                         insert_at_e(data);
                         displayList();
                         break;
-            case 4 : 
+            case 4 :    //calling function to delete the beginning element
                         delete_at_b();
                         displayList();
                         break;
-            case 5 : 
+            case 5 :    //calling function to delete at specified position
                         delete_at_m();
                         displayList();
                         break;
-            case 6 : 
+            case 6 :    //calling function to delete at end
                         delete_at_e();
                         displayList();
                         break;
-            case 7 :    
+            case 7 :    //calling function to search the element
                         printf("\nEnter element to search: ");
                         scanf("%d", &key);
                         index = search(key);
@@ -95,7 +100,7 @@ int main(){
                         else
                             printf("%d not found in the list.\n", key);
                         break;
-            case 8 :    
+            case 8 :    //for exiting the program
                         exit(0);
                         break;
             default :
@@ -103,14 +108,15 @@ int main(){
 
         }
     }
-    
+   
     return 0;
 }
+//function to display linked list
 void displayList()
 {
     struct node *temp;
 
-    
+   
     if(head == 0)
     {
         printf("List is empty.");
@@ -125,8 +131,9 @@ void displayList()
         }
     }
 }
+//fuction to insert at beginning of linked list
 void insert_at_b(int data){
-    
+   
     struct node *newnode;
     newnode = (struct node *)malloc(sizeof(struct node));
     if(newnode == NULL)
@@ -138,13 +145,13 @@ void insert_at_b(int data){
         newnode->data = data; // Link data part
         newnode->next = head; // Link address part
 
-        head = newnode; 
+        head = newnode;
         printf("DATA INSERTED SUCCESSFULLY\n");
           }
 
-    
+   
 }
-
+//function to delete the element in a specified position
 void delete_at_m(){
     struct node *temp, *nextnode;
 
@@ -165,6 +172,7 @@ void delete_at_m(){
         free(nextnode);
     }
 }
+//function to delete last element
 void delete_at_e(){
     struct node *temp, *prevnode;
 
@@ -188,7 +196,7 @@ void delete_at_e(){
         }
     }
 }
-
+//function to delete the beginning element
 void delete_at_b(){
     struct node *temp;
     if(head==0){
@@ -200,6 +208,7 @@ void delete_at_b(){
         free(temp);
     }
 }
+//function to insert the element at a particular specified position
 void insert_at_m(int data, int position){
     int i;
     struct node *newnode,*temp;
@@ -215,7 +224,7 @@ void insert_at_m(int data, int position){
 
         temp = head;
 
-        
+       
         for(i=2; i<=position-1; i++)
         {
             temp = temp->next;
@@ -227,7 +236,7 @@ void insert_at_m(int data, int position){
         if(temp != 0)
         {
             /* Link address part of new node */
-            newnode->next = temp->next; 
+            newnode->next = temp->next;
 
             /* Link address part of n-1 node */
             temp->next = newnode;
@@ -240,7 +249,7 @@ void insert_at_m(int data, int position){
         }
     }
 }
-
+//function to insert the element at the end position
 void insert_at_e(int data){
     struct node *newnode,*temp;
     newnode = (struct node *)malloc(sizeof(struct node));
@@ -250,12 +259,12 @@ void insert_at_e(int data){
     }
     else
     {
-        newnode->data = data; 
-        newnode->next = NULL; 
+        newnode->data = data;
+        newnode->next = NULL;
 
         temp = head;
 
-        
+       
         while(temp->next != 0)
             temp = temp->next;
 
@@ -265,6 +274,7 @@ void insert_at_e(int data){
     }
 
 }
+//function to search the element in linked list
 int search(int key){
     int index;
     struct node *curNode;
